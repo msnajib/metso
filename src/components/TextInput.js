cc9a315d8fddbae08463e68b1c1c218997a8edc4
Najib <macbook@MacBooks-MacBook-Pro.local>
2020-02-24 20:07:48 +0700

    Setup Folder

---
+++ src/components/TextInput.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const TextInput = () => {
+	return (
+		<View>
+			<Text></Text>
+		</View>
+	)
+}
+
+export default TextInput
+
+const styles = StyleSheet.create({})
